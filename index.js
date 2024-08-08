d7b7fe2b910980cd81fed868fe66777dda6e9527
Avviishek <avishekdastidar22@gmail.com>
2024-08-09 01:17:49 +0530

    Added new API endpoints for filtering

--- index.js
+++ index.js
@@ -40,7 +40,14 @@ app.get('/trucks', (req, res) => {
       });
     });
 app.get('/orders', (req, res) => {
-      const query = 'SELECT * FROM `TABLE 3`';  
+  const query = `
+  SELECT * 
+  FROM \`TABLE 3\` 
+  ORDER BY 
+      CAST(SUBSTRING(\`COL 7\`, 7, 4) AS UNSIGNED) * 10000 + 
+      CAST(SUBSTRING(\`COL 7\`, 4, 2) AS UNSIGNED) * 100 + 
+      CAST(SUBSTRING(\`COL 7\`, 1, 2) AS UNSIGNED) DESC
+`; 
       db.query(query, (err, results) => {
         if (err) {
           res.status(500).send(err);
@@ -52,7 +59,51 @@ app.get('/orders', (req, res) => {
        })
       });
     });    
-    
+    app.get('/orderaddress', (req, res) => {
+      const { address } = req.query;
+  
+      if (!address) {
+          return res.status(400).send('Address is required');
+      }
+  
+      const query = 'SELECT * FROM `TABLE 3` WHERE `COL 2` = ?';
+      db.query(query, [address], (err, results) => {
+          if (err) {
+              res.status(500).send(err);
+              return;
+          }
+          res.json(results);
+          results.forEach(result => {
+              console.log(result);
+          });
+      });
+  });
+  app.get('/orderdaterange', (req, res) => {
+    const { startDate, endDate } = req.query;
+
+    if (!startDate || !endDate) {
+        return res.status(400).send('Start date and end date are required');
+    }
+
+    // Convert dates from dd/mm/yyyy to yyyy-mm-dd for SQL
+    // const [startDay, startMonth, startYear] = startDate.split('/');
+    // const [endDay, endMonth, endYear] = endDate.split('/');
+
+    // const formattedStartDate = `${startYear}-${startMonth}-${startDay}`;
+    // const formattedEndDate = `${endYear}-${endMonth}-${endDay}`;
+
+    const query = 'SELECT * FROM `TABLE 3` WHERE `COL 7` BETWEEN ? AND ?';
+    db.query(query, [startDate, endDate], (err, results) => {
+        if (err) {
+            res.status(500).send(err);
+            return;
+        }
+        res.json(results);
+        results.forEach(result => {
+            console.log(result);
+        });
+    });
+});   
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
