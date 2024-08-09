a6c115927edc74f8eda2f9421d41a7dddb492bc3
Avviishek <avishekdastidar22@gmail.com>
2024-08-09 23:07:26 +0530

    added more endpoints

--- index.js
+++ index.js
@@ -85,14 +85,12 @@ app.get('/orders', (req, res) => {
         return res.status(400).send('Start date and end date are required');
     }
 
-    // Convert dates from dd/mm/yyyy to yyyy-mm-dd for SQL
-    // const [startDay, startMonth, startYear] = startDate.split('/');
-    // const [endDay, endMonth, endYear] = endDate.split('/');
+   
 
-    // const formattedStartDate = `${startYear}-${startMonth}-${startDay}`;
-    // const formattedEndDate = `${endYear}-${endMonth}-${endDay}`;
-
-    const query = 'SELECT * FROM `TABLE 3` WHERE `COL 7` BETWEEN ? AND ?';
+    const query = `SELECT * FROM \`TABLE 3\`   WHERE \`COL 7\` BETWEEN ? AND ? ORDER BY 
+      CAST(SUBSTRING(\`COL 7\`, 7, 4) AS UNSIGNED) * 10000 + 
+      CAST(SUBSTRING(\`COL 7\`, 4, 2) AS UNSIGNED) * 100 + 
+      CAST(SUBSTRING(\`COL 7\`, 1, 2) AS UNSIGNED) ASC`;
     db.query(query, [startDate, endDate], (err, results) => {
         if (err) {
             res.status(500).send(err);
@@ -103,7 +101,51 @@ app.get('/orders', (req, res) => {
             console.log(result);
         });
     });
-});   
+}); 
+app.get('/orderdateaddress', (req, res) => {
+  const { startDate, endDate, address } = req.query;
+
+  if (!startDate || !endDate) {
+      return res.status(400).send('Start date and end date are required');
+  }
+  if (!address) {
+    return res.status(400).send('Address is required');
+}
+
+ 
+
+  const query = `SELECT * FROM \`TABLE 3\`   WHERE \`COL 7\` BETWEEN ? AND ? 
+    AND \`COL 2\` = ?
+    ORDER BY 
+    CAST(SUBSTRING(\`COL 7\`, 7, 4) AS UNSIGNED) * 10000 + 
+    CAST(SUBSTRING(\`COL 7\`, 4, 2) AS UNSIGNED) * 100 + 
+    CAST(SUBSTRING(\`COL 7\`, 1, 2) AS UNSIGNED) ASC`;
+  db.query(query, [startDate, endDate , address], (err, results) => {
+      if (err) {
+          res.status(500).send(err);
+          return;
+      }
+      res.json(results);
+      results.forEach(result => {
+          console.log(result);
+      });
+  });
+}); 
+app.get('/address', (req, res) => {
+  const query = 'SELECT DISTINCT `COL 2` FROM `TABLE 3`'; 
+
+  db.query(query, (err, results) => {
+      if (err) {
+          res.status(500).send(err);
+          return;
+      }
+      res.json(results);
+      results.forEach(result => {
+          console.log(result);
+      });
+  });
+});  
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
