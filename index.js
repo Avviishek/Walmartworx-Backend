b3f2e3f5bdcce6ac25e0ae5957955cb0e7f040d2
Avviishek <avishekdastidar22@gmail.com>
2024-08-13 04:22:37 +0530

    more endpoints added

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ const app = express();
 const port = 3001;
 
 app.use(express.json());
-app.use(cors());
+const corsOptions = {
+  origin: ['http://localhost:3000', 'https://warehouseworx.vercel.app/'], // Allow only these origins
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
+  credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+};
+
+app.use(cors(corsOptions));
 
 const db = mysql.createConnection({
   host: 'sql12.freesqldatabase.com',
@@ -58,7 +64,36 @@ app.get('/orders', (req, res) => {
             console.log(result) ;
        })
       });
-    });    
+    }); 
+    app.get('/searchorders', (req, res) => {
+      const orderId = req.query.order_id;
+    
+      let query = `
+        SELECT * 
+        FROM \`TABLE 3\` 
+        ORDER BY 
+          CAST(SUBSTRING(\`COL 7\`, 7, 4) AS UNSIGNED) * 10000 + 
+          CAST(SUBSTRING(\`COL 7\`, 4, 2) AS UNSIGNED) * 100 + 
+          CAST(SUBSTRING(\`COL 7\`, 1, 2) AS UNSIGNED) DESC
+      `;
+    
+      // If order_id is provided, add a WHERE clause to the query
+      if (orderId) {
+        query += ` WHERE \`COL 1\` = ?`;
+      }
+    
+      db.query(query, [orderId], (err, results) => {
+        if (err) {
+          res.status(500).send(err);
+          return;
+        }
+        res.json(results);
+        results.forEach(result => {
+          console.log(result);
+        });
+      });
+    });
+       
     app.get('/orderaddress', (req, res) => {
       const { address } = req.query;
   
@@ -167,16 +202,16 @@ app.get('/assignedorder', (req, res) => {
   }
 
   // Determine the table to query based on the address
-  let tableName;
-  if (address.toLowerCase() === 'delhi') {
-      tableName = 'TABLE 4';
-  } else if (address.toLowerCase() === 'hyderabad') {
-      tableName = 'TABLE 5';
-  } else {
-      return res.status(400).send('Invalid address provided');
-  }
+  let query;
 
-  const query = `SELECT * FROM \`${tableName}\``;
+  if (address === 'delhi') {
+    query = 'SELECT * FROM `TABLE 4`';
+  } else if (address === 'hyderabad') {
+    query = 'SELECT * FROM `TABLE 5`';
+  } else {
+    query = 'SELECT * FROM `TABLE 3` WHERE `COL 2` = ?'
+  }
+  if(address == 'delhi' || address == 'hyderabad'){
   db.query(query, (err, results) => {
       if (err) {
           res.status(500).send(err);
@@ -186,7 +221,19 @@ app.get('/assignedorder', (req, res) => {
       results.forEach(result => {
           console.log(result);
       });
+  });}else{
+    db.query(query,[address], (err, results) => {
+      if (err) {
+          res.status(500).send(err);
+          return;
+      }
+      res.json(results);
+      results.forEach(result => {
+          console.log(result);
+      });
   });
+  }
+
 });
 app.get('/assigned', (req, res) => {
   const query = 'SELECT DISTINCT `COL 2` FROM `TABLE 3`'; 
@@ -230,7 +277,88 @@ app.get('/assigntruck', (req, res) => {
           "date assigned": currentDate,
           "volume": 27.65
       };
-  } else {
+  } else if (address.toLowerCase() === 'bangalore') {
+    responseData = {
+        "vehicle reg no": "KA90AD6939", 
+        "driver name": "Kunal",
+        "mobile no": "7194039976",
+        "date assigned": currentDate,
+        "volume": 30
+    };
+  }else if (address.toLowerCase() === 'silchar') {
+    responseData = {
+        "vehicle reg no": "AS98EO3461", 
+        "driver name": "Atharva",
+        "mobile no": "9582335390",
+        "date assigned": currentDate,
+        "volume": 30
+    };
+  }else if (address.toLowerCase() === 'pune') {
+    responseData = {
+        "vehicle reg no": "MH99RW2846", 
+        "driver name": "Gaurav",
+        "mobile no": "7174104671",
+        "date assigned": currentDate,
+        "volume": 30
+    };
+  }else if (address.toLowerCase() === 'noida') {
+    responseData = {
+        "vehicle reg no": "UP12MR7004", 
+        "driver name": "Krishna",
+        "mobile no": "9393678640",
+        "date assigned": currentDate,
+        "volume": 18
+    };
+  }else if (address.toLowerCase() === 'chennai') {
+    responseData = {
+        "vehicle reg no": "TS36VH9939", 
+        "driver name": "Vikas",
+        "mobile no": "7240121780",
+        "date assigned": currentDate,
+        "volume": 27.65
+    };
+  }else if (address.toLowerCase() === 'mumbai') {
+    responseData = {
+      "vehicle reg no": "MH99RW2846", 
+        "driver name": "Gaurav",
+        "mobile no": "7174104671",
+        "date assigned": currentDate,
+        "volume": 30
+    };
+  }else if (address.toLowerCase() === 'kolkata') {
+    responseData = {
+        "vehicle reg no": "WB12UR9703", 
+        "driver name": "Anirudh",
+        "mobile no": "8661183365",
+        "date assigned": currentDate,
+        "volume": 27.65
+    };
+  }else if (address.toLowerCase() === 'guwahati') {
+    responseData = {
+      "vehicle reg no": "AS98EO3461", 
+      "driver name": "Atharva",
+      "mobile no": "9582335390",
+      "date assigned": currentDate,
+      "volume": 30
+    };
+  }else if (address.toLowerCase() === 'darbangha') {
+    responseData = {
+        "vehicle reg no": "TS36VH9939", 
+        "driver name": "Vikas",
+        "mobile no": "7240121780",
+        "date assigned": currentDate,
+        "volume": 27.65
+    };
+  }else if (address.toLowerCase() === 'patna') {
+    responseData = {
+        "vehicle reg no": "TS36VH9939", 
+        "driver name": "Vikas",
+        "mobile no": "7240121780",
+        "date assigned": currentDate,
+        "volume": 27.65
+    };
+  }
+  else {
       return res.status(400).send('Invalid address provided');
   }
   res.json(responseData);
@@ -297,7 +425,82 @@ app.get('/recentorders', (req, res) => {
        })
       });
     });  
+app.get('/batch' , (req,res)=>{
+  const query = 'SELECT * FROM `TABLE 8`';
+  db.query(query, (err, results) => {
+    if (err) {
+      res.status(500).send(err);
+      return;
+    }
+    res.json(results);
+  });
+}) ;
+app.post('/addbatch' , (req,res)=>{
+  const batch = req.body;
 
+  if (!batch || Object.keys(batch).length === 0) {
+      return res.status(400).send('Order data is required');
+  }
+
+  const statusOptions = ["Dispatched", "Not Yet Dispatched"];
+  const randomStatus = statusOptions[Math.floor(Math.random() * statusOptions.length)];
+  const query = 'INSERT INTO `TABLE 8` (`COL 1`, `COL 2`, `COL 3`,`COL 4`,`COL 5`,`COL 6` ) VALUES (?, ?, ?,?,?,?)';
+  const values = [
+    batch["driver name"],
+    batch["vehicle reg no"],
+    batch["mobile no"],
+    batch["volume"],
+    randomStatus,
+    batch["address"],
+      
+  ];
+  db.query(query, values, (err, result) => {
+    if (err) {
+      res.status(500).send(err);
+      return;
+    }
+    res.status(201).json({ message: 'Data added successfully', insertedId: result.insertId });
+  });
+});
+app.get('/batchorder' , (req,res)=>{
+  const { address } = req.query;
+  if (!address) {
+    return res.status(400).send('Address is required');
+}
+
+const query = 'SELECT * FROM `TABLE 3` WHERE `COL 2` = ? LIMIT 50';
+db.query(query, [address], (err, results) => {
+    if (err) {
+        res.status(500).send(err);
+        return;
+    }
+    res.json(results);
+    results.forEach(result => {
+        console.log(result);
+    });
+});
+
+});
+app.get('/predictedSales' , (req,res)=>{
+  const {address} = req.query; 
+  let query ; 
+   if(address.toLowerCase() === 'guwahati'){
+    query = 'SELECT * FROM `TABLE 9`' ;
+   }else{
+    query = 'SELECT * FROM `TABLE 10`' ;
+   }
+   //query = 'SELECT *FROM `TABLE 9`' ;
+  db.query(query, (err, results) => {
+    if (err) {
+      res.status(500).send(err);
+      return;
+    }
+    res.json(results);
+   results.forEach(result=>{
+        console.log(result) ;
+   })
+  });
+});
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
